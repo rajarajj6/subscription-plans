7bae7b0ebd6729b944a7367c784968cb6f241c30
rajarajj6 <96234410+rajarajj6@users.noreply.github.com>
2022-02-17 12:45:50 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,64 @@
+ import Plans from './plans';
+ import "./App.css";
+
+ function App() {
+   const plans = [
+     {
+       name: "Free",
+       price: 0,
+       features: [
+         { name: "Single User", enabled: true },
+         { name: "5GB Storage", enabled: true },
+         { name: "Unlimited Public Projects", enabled: true },
+         { name: "Community Access", enabled: true },
+         { name: "Unlimited Private Projects", enabled: false },
+         { name: "Dedicated Phone Support", enabled: false },
+         { name: "Free Subdomain", enabled: false },
+         { name: "Monthly Status Reports", enabled: false }
+       ],
+     },
+     {
+       name: "Plus",
+       price: 9,
+       features: [
+         { name: "Single User", enabled: true },
+         { name: "5GB Storage", enabled: true },
+         { name: "Unlimited Public Projects", enabled: true },
+         { name: "Community Access", enabled: true },
+         { name: "Unlimited Private Projects", enabled: true },
+         { name: "Dedicated Phone Support", enabled: true },
+         { name: "Free Subdomain", enabled: true },
+         { name: "Monthly Status Reports", enabled: false }
+       ],
+     },
+     {
+       name: "pro",
+       price: 49,
+       features: [
+         { name: "Single User", enabled: true },
+         { name: "5GB Storage", enabled: true },
+         { name: "Unlimited Public Projects", enabled: true },
+         { name: "Community Access", enabled: true },
+         { name: "Unlimited Private Projects", enabled: true },
+         { name: "Dedicated Phone Support", enabled: true },
+         { name: "Free Subdomain", enabled: true },
+         { name: "Monthly Status Reports", enabled: true }
+       ],
+     },
+   ];
+   return (
+     <section className="pricing py-5">
+       <div className="container">
+         <div className="row">
+           {
+             plans.map((plan, index) => {
+               return <Plans key={index} name={plan.name} price={plan.price} features={plan.features} />
+             })
+           }
+         </div>
+       </div>
+     </section>
+   );
+        }
+ 
+ export default App ;
